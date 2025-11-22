6d19b63ecb7c105f42d7136b5b7133beecbcc163
Rafa Silva <rafaelldasilva1984@gmail.com>
2025-11-21 23:59:43 -0300

    Update simulate-logs.sh

--- AULA01-LOGS/ingest/simulate-logs.sh
+++ AULA01-LOGS/ingest/simulate-logs.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-curl -X POST "http://localhost:9200/observa-logs-default/_bulk?pipeline=pipeline-logs" -H 'Content-Type: application/x-ndjson' --data-binary @../data/logs-simulados.json
+
+echo "🔥 Enviando logs para o Elasticsearch..."
+
+curl -k -X POST "https://localhost:9200/_bulk" \
+  -H "Content-Type: application/x-ndjson" \
+  --user elastic:changeme \
+  --data-binary "@../data/logs-bulk.ndjson"
+
+echo
+echo "✅ Envio finalizado!"
