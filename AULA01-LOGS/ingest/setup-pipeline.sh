7433222e4509e6b30222cba258d16eb20a975904
Rafa Silva <rafaelldasilva1984@gmail.com>
2025-11-22 12:07:48 -0300

    Delete AULA01-LOGS directory

--- AULA01-LOGS/ingest/setup-pipeline.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -X PUT "http://localhost:9200/_ingest/pipeline/pipeline-logs" -H 'Content-Type: application/json' -d @pipeline-logs.json
--- AULA01-LOGS/ingest/setup-template.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-curl -X PUT "http://localhost:9200/_index_template/observa-logs-template" -H 'Content-Type: application/json' -d @template.json
