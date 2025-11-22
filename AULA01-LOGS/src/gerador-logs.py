590d939d680f0205dc8958e0ff667b01e0970de3
Rafa Silva <rafaelldasilva1984@gmail.com>
2025-11-21 23:59:17 -0300

    Update gerador-logs.py

--- AULA01-LOGS/src/gerador-logs.py
+++ AULA01-LOGS/src/gerador-logs.py
@@ -6,27 +6,26 @@ services = ["auth", "checkout", "search", "orders", "catalog"]
 levels = ["INFO", "WARN", "ERROR"]
 endpoints = ["/login", "/pay", "/cart", "/item", "/search"]
 
-dataset = []
-
-for i in range(10000):
-    ts = datetime.datetime.utcnow().isoformat()
-    latency = random.randint(10, 2000)
-    level = random.choices(levels, weights=[80, 15, 5])[0]
-
-    dataset.append({
-        "timestamp": ts,
-        "level": level,
-        "service": random.choice(services),
-        "endpoint": random.choice(endpoints),
-        "latency": latency,
-        "host": f"server-{random.randint(1,10)}"
-    })
-
-# grava o arquivo NDJSON na pasta ../data
-output_path = "../data/logs-simulados.json"
+output_path = "../data/logs-bulk.ndjson"
 
 with open(output_path, "w", encoding="utf-8") as f:
-    for line in dataset:
-        f.write(json.dumps(line) + "\n")
+    for i in range(10000):
+        ts = datetime.datetime.utcnow().isoformat()
+        latency = random.randint(10, 2000)
+        level = random.choices(levels, weights=[80, 15, 5])[0]
 
-print(f"✅ Arquivo gerado com sucesso em: {output_path}")
+        doc = {
+            "timestamp": ts,
+            "level": level,
+            "service": random.choice(services),
+            "endpoint": random.choice(endpoints),
+            "latency": latency,
+            "host": f"server-{random.randint(1,10)}"
+        }
+
+        action = { "index": { "_index": "logs-aula01" } }
+
+        f.write(json.dumps(action) + "\n")
+        f.write(json.dumps(doc) + "\n")
+
+print(f"✅ BULK NDJSON criado em: {output_path}")
