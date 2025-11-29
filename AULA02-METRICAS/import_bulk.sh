681883286d415314925db3ff4d6ae0c6e430fa39
Rafa Silva <rafaelldasilva1984@gmail.com>
2025-11-29 19:54:48 -0300

    Delete AULA02-METRICAS/import_bulk.sh

--- AULA02-METRICAS/import_bulk.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-FILE="metricas_bulk.jsonl"
-INDEX="metricas-lab"
-echo "Importando via BULK API..."
-curl -k -X POST "http://localhost:9200/_bulk" -H "Content-Type: application/x-ndjson" --data-binary "@${FILE}"
-echo "Importação finalizada."
